3ea0c04480b407e1a613382b69bcfdfd4e2640ee
wrunika <2805nika@gmail.com>
2022-12-05 12:09:01 +0300

    create components Rating, Star and Accordion inside App

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,41 @@ function App() {
   return (
     <div className="App">
      Hello, samurai! Let's go!
+        <Rating />
+        <Accordion />
     </div>
   );
+}
+
+function Rating() {
+    return (
+        <div>
+            <Star />
+            <Star />
+            <Star />
+            <Star />
+            <Star />
+        </div>
+    );
+}
+
+function Star() {
+    return (
+        <div>star</div>
+    );
+}
+
+function Accordion() {
+    return (
+        <div>
+            <h3>Меню</h3>
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>2</li>
+            </ul>
+        </div>
+    );
 }
 
 export default App;
