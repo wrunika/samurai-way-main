d8254c0f7c8cff06c6e9376be71ea8ebc6d40823
wrunika <2805nika@gmail.com>
2022-12-05 14:30:47 +0300

    feat: create components AccordionTitle and AccordionBody into App

--- src/App.tsx
+++ src/App.tsx
@@ -46,13 +46,27 @@ function Accordion() {
     console.log('accordion rendering')
     return (
         <div>
-            <h3>Меню</h3>
-            <ul>
-                <li>1</li>
-                <li>2</li>
-                <li>2</li>
-            </ul>
+            <AccordionTitle/>
+            <AccordionBody/>
         </div>
+    );
+}
+
+function AccordionTitle() {
+    console.log('accordionTitle rendering')
+    return(
+        <h3>Меню</h3>
+    );
+}
+
+function AccordionBody() {
+    console.log('accordionBody rendering')
+    return(
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>2</li>
+        </ul>
     );
 }
 
